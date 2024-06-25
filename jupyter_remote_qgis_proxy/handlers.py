baf0d373c2f965a60bc6fe038bb04cacc8df8cf5
Tarashish Mishra <sunu@sunu.in>
2024-06-25 10:08:14 +0530

    clean up formatting

--- jupyter_remote_qgis_proxy/handlers.py
+++ jupyter_remote_qgis_proxy/handlers.py
@@ -2,7 +2,7 @@ from jupyter_remote_desktop_proxy.handlers import DesktopHandler
 from tornado import web
 from tornado.ioloop import IOLoop
 
-from .utils import is_process_running, start_qgis
+from .utils import start_qgis
 
 
 class QgisHandler(DesktopHandler):
@@ -14,7 +14,9 @@ class QgisHandler(DesktopHandler):
         project_name = self.get_argument("project_name", "new project")
         layer_name = self.get_argument("layer_name", "Vector Layer")
 
-        if not is_process_running("websockify"):
-            IOLoop.current().call_later(3, lambda:start_qgis(action, url, project_name, layer_name))
-        
-
+        IOLoop.current().call_later(
+            delay=3,
+            callback=lambda: start_qgis(
+                action=action, url=url, project_name=project_name, layer_name=layer_name
+            ),
+        )
--- jupyter_remote_qgis_proxy/utils.py
+++ jupyter_remote_qgis_proxy/utils.py
@@ -6,42 +6,43 @@ from .qgis.utils import open_qgis
 
 
 def is_process_running(process_name):
-  """
-  Checks if a process with the given name is running.
-  """
-  for process in psutil.process_iter():
-    try:
-      if process.name().lower() == process_name.lower():
-        return True
-    except (psutil.NoSuchProcess, psutil.AccessDenied):
-      pass
-  return False
+    """
+    Checks if a process with the given name is running.
+    """
+    for process in psutil.process_iter():
+        try:
+            if process.name().lower() == process_name.lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            pass
+    return False
 
 
 def wait_for_condition(condition, timeout=10):
-  """
-  Waits for a condition to be true for a specified timeout.
+    """
+    Waits for a condition to be true for a specified timeout.
 
-  Args:
-      condition: A callable that returns True when the condition is met.
-      timeout: The maximum time in seconds to wait (default: 10).
+    Args:
+        condition: A callable that returns True when the condition is met.
+        timeout: The maximum time in seconds to wait (default: 10).
 
-  Returns:
-      True if the condition becomes true before the timeout, False otherwise.
-  """
-  start_time = time.time()
-  while time.time() - start_time < timeout:
-    if condition():
-      return True
-    time.sleep(0.5)
-  return False
+    Returns:
+        True if the condition becomes true before the timeout, False otherwise.
+    """
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        if condition():
+            return True
+        time.sleep(0.5)
+    return False
 
 
 def start_qgis(action, url, project_name, layer_name):
     """Starts QGIS and waits for websockify to start if necessary."""
     logger.info("Waiting for websockify to start")
-    is_websockify_running = lambda: is_process_running("websockify")
-    running = wait_for_condition(is_websockify_running, timeout=10)
+    running = wait_for_condition(
+        condition=lambda: is_process_running(process_name="websockify"), timeout=10
+    )
     logger.info(f"Websockify is running: {running}")
     logger.info("Starting QGIS")
     if action and url:
